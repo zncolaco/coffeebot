96a3441a2f338504407b163466814148fa1f69a3
HJ Leong <HJ Leong>
2017-03-18 22:01:47 +1300

    Simple slack bot for hello and unacceptable.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# coffeebot
+# Uber Coffee Hipchat Bot
+
+## Prereqs
+
+* [nodejs](https://nodejs.org/en/)
+
+## Get started
+
+1. Clone this repo
+1. Get dependencies: `npm install`
+1. Create a new user for the bot, by going to `https://yourroom.slack.com/services/new/bot` (take note of the API Token)
+1. Run with the API token as an environmental variable: `SLACK_TOKEN=xxx-xxx-xxx npm start` (this is Windows specific)
+1. Add the bot to your room `/invite @bot`
+
+## References
+
+* [SlackJS](https://www.npmjs.com/package/slack)
+* [Slack Real Time Messaging API](https://api.slack.com/rtm)
---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict'
+
+var slack = require('slack');
+var express = require('express');
+var app = express();
+
+let bot = slack.rtm.client();
+let token = process.env.SLACK_TOKEN;
+
+app.get('/', function (req, res) {
+	res.send('Uber Coffee Bot is up and running!!!');
+})
+ 
+app.listen(3000, (err) => {
+	if (err) throw err
+
+	console.log('Started ...');
+	bot.listen({token});
+});
+
+// Events
+bot.message(function (message) {
+	
+	// Only respond to human messages
+	if (!message.user || message.bot_id) return;
+	
+	var rawMessage = message.text;
+	
+	// hello
+	if (/^h(i)+(\s[\S]*)?$|^h[e]+l[l]+[o]+(\s[\S]*)?$/i.test(rawMessage)) {
+		postImage(message.channel, 'https://media.riffsy.com/images/44aed745f86834ab57d88bc010bb036c/tenor.gif');
+	}
+	
+	// unacceptable
+	if (/^unacceptable/i.test(rawMessage)) {
+		postImage(message.channel, 'https://media.giphy.com/media/3eKdC7REvgOt2/giphy.gif');
+	}
+	
+});
+
+function postImage(channel, imageUrl) {
+	slack.chat.postMessage({
+		token: token,
+		as_user: true,
+		channel: channel,
+		text: '',
+		attachments: [
+			{
+				text: '',
+				image_url: imageUrl
+			}
+		]
+	}, function (err, data) {
+		if (err) console.log(err);
+	})
+}
+
+function postMessage(channel, message) {
+	slack.chat.postMessage({
+		token: token,
+		as_user: true,
+		channel: channel,
+		text: message
+	}, function (err, data) {
+		if (err) console.log(err);
+	})
+}
