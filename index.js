9cc823d6e97f690c73ca72f21ddd6f09dd98400b
HJ Leong <HJ Leong>
2017-04-06 01:17:20 +1200

    Enable karma counting.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 var slack = require('slack');
 var express = require('express');
-var request = require('request')
+var request = require('request');
+var sqlite3 = require('sqlite3');
 var app = express();
 
 let bot = slack.rtm.client();
 let token = process.env.SLACK_TOKEN;
+var db;
 
 app.get('/', function (req, res) {
 	res.send('Uber Coffee Bot is up and running!!!');
@@ -17,6 +19,11 @@ app.listen(3000, (err) => {
 
 	console.log('Started ...');
 	bot.listen({token});
+	
+	db = new sqlite3.Database('karma.db');
+	db.serialize(function() {
+		db.run("CREATE TABLE if not exists karma (name TEXT PRIMARY KEY, score INTEGER)");
+	});
 });
 
 bot.hello(message=> {
@@ -119,6 +126,85 @@ bot.message(function (message) {
 		postMessageAs(message.channel, '"' + suchPhrase[1] + '"', 'doge', 'https://ih0.redbubble.net/image.28632195.0283/flat,800x800,070,f.jpg');	
 	}
 	
+	// karma
+	if (/(?:[^\s]+|".*")\s?(?:\+[\+]+|-[-]+)/i.test(rawMessage)) {
+		
+		var karmarama = /([^\s\+-]+|".*")\s?(\+[\+]+|-[-]+)/ig.exec(rawMessage);
+		var name = karmarama[1];
+		var karmaPoints = convertToPoints(karmarama[2]);
+				
+		// control yourself
+		if (name == "<@" + message.user + ">") {
+			if (karmaPoints > 0) {
+				postMessage(message.channel, "Don't be a weasel <@" + message.user + ">.");
+			} else {
+				postMessage(message.channel, "Aww, don't be so hard on yourself.");
+			}
+			return;
+		}
+		
+		db.serialize(function() {
+			// Get the current karma
+			var currentScore = 0;
+			var newScore = 0;
+			db.get("SELECT name, score FROM karma WHERE name = ?", name, function(err, row) {
+				if (row != undefined) {
+					currentScore = row.score;
+				}
+				// Insert or update the new karma value
+				newScore = currentScore + karmaPoints;
+				db.run("INSERT OR REPLACE INTO karma (name, score) VALUES (?,?)", [name, newScore], function(err) {
+					if (err) throw err;
+					postMessage(message.channel, ">_" + name + "'s karma is now " + newScore + "_");
+				});
+			});
+		});
+				
+	}
+	
+	// Reset all the things
+	if (/^coffeebotresetallthekarmaplz$/i.test(rawMessage)) {
+		db.serialize(function() {
+			db.run("DELETE FROM karma; VACUUM", function(err) {
+				if (err) throw err;
+				postMessage(message.channel, ">_All your karma has been reset._");
+			});
+		});
+	}
+	
+	// Check the score
+	if (/^karma of (.*)$/i.test(rawMessage)) {
+		var query = /^karma of (.*)$/ig.exec(rawMessage);
+		var name = query[1];
+		var score = 0;
+		db.serialize(function() {
+			db.get("SELECT score FROM karma WHERE name = ?", name, function(err, row) {
+				if (err) throw err;
+				if (row) score = row.score;
+				postMessage(message.channel, ">_" + name + " has " + score + " karma._");
+			});
+		});
+	}
+	
+	// Check the score
+	if (/^(top|bottom) karma$/i.test(rawMessage)) {
+		var query = /^(top|bottom) karma$/ig.exec(rawMessage);
+		var topBottom = query[1];
+		var sortOrder = "ASC";
+		if (topBottom.toLowerCase() == "top" ) sortOrder = "DESC"
+
+		db.serialize(function() {
+			db.all("SELECT name, score FROM karma ORDER BY SCORE " + sortOrder + " LIMIT 5", function(err, rows) {
+				if (err) throw err;
+				var results = ">>> The " + topBottom.toLowerCase() + " things with karma are: \n"
+				for (var i = 0; i < rows.length; i++) {
+					results += "\t" + rows[i].score + "\t-\t" + rows[i].name + "\n";
+				}
+				postMessage(message.channel, results);
+			});
+		});
+	}
+	
 });
 
 function postMessageAndImage(channel, message, imageUrl) {
@@ -163,4 +249,17 @@ function postMessageAs(channel, message, username, userIconUrl) {
 	}, function (err, data) {
 		if (err) console.log(err);
 	})
+}
+
+function convertToPoints(karma) {
+	// Throttle to +/- 5
+	karma = karma.substring(0,6); 
+	var points = karma.length - 1;
+	if (karma.indexOf('+') > -1) {
+		return points;
+	}
+	if (karma.indexOf('-') > -1) {
+		return 0 - points;
+	}
+	return 0;
 }
